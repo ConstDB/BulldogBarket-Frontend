18720d8f7d9b605d6f421a91a6a8b93514490080
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-12-10 22:47:18 +0800

    set up tailwind and adjust styles to some pages

---
+++ src/components/MarketFeed/BulkOrderModal.jsx
@@ -0,0 +1,11 @@
+import "../../styles/MarketFeed/BulkOrderModal.css"
+
+export default function BulkOrderModal(){
+    return (
+         <div className="overlay">
+            <div>
+                 
+            </div> 
+        </div>
+    )
+}
