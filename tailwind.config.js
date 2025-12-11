757c7ed5b404f915597a20b51b4eea7c81eb7d1b
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-12-12 03:58:13 +0800

    add OrderModal and configure shadcn, tanstack query, and axios

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,20 @@ export default{
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6b94941 (add OrderModal and configure shadcn, tanstack query, and axios)
     extend: {
       zIndex: {
         '9999': '9999',
       }
     }, 
+<<<<<<< HEAD
 =======
     extend: {}, 
 >>>>>>> a7090c2 (add shadcn on the current setup)
+=======
+>>>>>>> 6b94941 (add OrderModal and configure shadcn, tanstack query, and axios)
   },
   plugins: [],
 };
