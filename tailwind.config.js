3529f7065fe08da6835dd7fa503b00af77251ec3
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-12-11 21:59:22 +0800

    add shadcn on the current setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@
 export default{
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+<<<<<<< HEAD
     extend: {
       zIndex: {
         '9999': '9999',
       }
     }, 
+=======
+    extend: {}, 
+>>>>>>> a7090c2 (add shadcn on the current setup)
   },
   plugins: [],
 };
