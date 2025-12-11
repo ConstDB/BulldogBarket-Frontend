b018a5f441402cfed17e1082d23830b2ea663689
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-12-12 03:58:13 +0800

    add OrderModal and configure shadcn, tanstack query, and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import MarketFeed from "./pages/MarketFeed";
 import Home from "./pages/home";
@@ -13,22 +14,26 @@ import SellerDashboard from "./pages/dashboard";
 
 
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
-      <Header />
-        <Routes>
-          <Route path="/marketfeed" element={<MarketFeed />} />
-          <Route path="/profile" element={<UserProfileEdit />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/post-product" element={<PostProduct />} />
-          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-        </Routes>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <>
+        <Header />
+          <Routes>
+            <Route path="/marketfeed" element={<MarketFeed />} />
+            <Route path="/profile" element={<UserProfileEdit />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/post-product" element={<PostProduct />} />
+            <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+          </Routes>
+      </>
+    </QueryClientProvider>
   );
 }
 
--- src/components/MarketFeed/MultipleItemPost.jsx
+++ src/components/MarketFeed/MultipleItemPost.jsx
@@ -10,7 +10,7 @@ import CountVotes from "../../assets/countvotes.svg";
 import profileIcon from "../../assets/profileicon.svg";
 import foodImage from "../../assets/food.svg";
 import useModalManager from "../../hooks/useModalManager";
-import BulkOrderModal from "../../modals/BulkOrderModal";
+import BulkOrderModal from "../../modals/OrderModal";
  
 const API_BASE = "http://127.0.0.1:3000/api/v1";
  
@@ -237,6 +237,7 @@ export default function MultipleItemPost({ post }) {
         <span className="mip-stat">
           <img src={CountVotes} alt="Count" className="mip-stat-icon" /> {upvotesLocal} upvotes
         </span>
+  
         <span className="mip-stat">{commentCount} comments</span>
       </div>
  
